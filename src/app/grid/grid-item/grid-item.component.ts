0fa6d2ec57b799ba3853d13dcf2b061e2c594aaa
Rayan MARMAR <rayan.marmar@etu.unistra.fr>
2024-01-12 17:56:34 +0100

    :recycle: Refactor grid component

---
+++ src/app/grid/grid-item/grid-item.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'grid-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './grid-item.component.html',
+  styleUrl: './grid-item.component.css'
+})
+export class GridItemComponent {
+
+}
--- src/app/grid/grid.component.ts
+++ src/app/grid/grid.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import {GridItemComponent} from "./grid-item/grid-item.component";
+import {NgForOf} from "@angular/common";
 
 @Component({
-  selector: 'app-grid',
+  selector: 'grid-table',
   standalone: true,
-  imports: [],
+  imports: [GridItemComponent, NgForOf],
   templateUrl: './grid.component.html',
   styleUrl: './grid.component.css'
 })
-export class GridComponent {
-
+export class GridComponent{
+  private numRows = 10;
+  private numCols = 10;
+  constructor() {
+  }
+  get numRowsArray(): number[] {
+    return Array(this.numRows * this.numCols).fill(0).map((_, index) => index);
+  }
 }
