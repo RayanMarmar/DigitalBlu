d5fcb8669010439189fca5419130854303aa8b00
Rayan MARMAR <rayan.marmar@etu.unistra.fr>
2024-01-15 13:51:16 +0100

    :sparkles: draw lines on mouse drag

--- src/app/canvas/canvas.component.ts
+++ src/app/canvas/canvas.component.ts
@@ -11,6 +11,9 @@ export class CanvasComponent  implements AfterViewInit{
 // its important myCanvas matches the variable name in the template
   @ViewChild('myCanvas', {static: true}) canvas!: ElementRef<HTMLCanvasElement>;
   context: CanvasRenderingContext2D | null = null;
+  oldX : number = 0;
+  oldY : number = 0;
+  mouseClicked : boolean = false;
 
   constructor() {
   }
@@ -20,15 +23,36 @@ export class CanvasComponent  implements AfterViewInit{
   }
   drawPoint(event: MouseEvent) : void {
     const rect : DOMRect = this.canvas.nativeElement.getBoundingClientRect();
-    let x :number = event.clientX - rect.left;
-    let y :number = event.clientY - rect.top;
+    this.oldX = event.clientX - rect.left;
+    this.oldY = event.clientY - rect.top;
+    this.mouseClicked = true;
     if (this.context) {
-      this.context.fillRect(x - 2, y - 2,5,5);
+      this.context.fillRect(this.oldX - 2, this.oldY - 2,5,5);
     } else {
       console.error('Context is null.');
     }
   }
 
+  drawLine(event: MouseEvent) : void {
+    if(!this.mouseClicked)
+      return;
+    const rect : DOMRect = this.canvas.nativeElement.getBoundingClientRect();
+    let x :number = event.clientX - rect.left;
+    let y :number = event.clientY - rect.top;
+    if (this.context) {
+      this.context.clearRect(0,0,rect.width,rect.height);
+      this.context.beginPath()
+      this.context.moveTo(this.oldX, this.oldY);
+      this.context.lineTo(x, y);
+      this.context.stroke();
+    } else {
+      console.error('Context is null.');
+    }
+  }
+
+  onMouseUp() : void {
+    this.mouseClicked = false;
+  }
   setCanvasSize(): void {
     if (this.context) {
       // Set canvas dimensions to match window size
