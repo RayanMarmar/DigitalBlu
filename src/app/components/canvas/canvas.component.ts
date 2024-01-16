a16d7188f9c8bc9b14d9d202c22c226d49f90167
Rayan MARMAR <rayan.marmar@etu.unistra.fr>
2024-01-16 22:53:04 +0100

    :sparkles: improved line drawing

--- src/app/components/canvas/canvas.component.ts
+++ src/app/components/canvas/canvas.component.ts
@@ -1,143 +1,159 @@
-import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, HostListener, ViewChild} from '@angular/core';
 import {Mouse} from "../../models/mouse";
 import {Line} from "../../models/line";
 import {Point} from "../../models/point";
 
 @Component({
-  selector: 'app-canvas',
-  standalone: true,
-  imports: [],
-  templateUrl: './canvas.component.html',
-  styleUrl: './canvas.component.css'
+    selector: 'app-canvas',
+    standalone: true,
+    imports: [],
+    templateUrl: './canvas.component.html',
+    styleUrl: './canvas.component.css'
 })
-export class CanvasComponent  implements AfterViewInit{
+export class CanvasComponent implements AfterViewInit {
 // its important myCanvas matches the variable name in the template
-  @ViewChild('myCanvas', {static: true}) private canvas!: ElementRef<HTMLCanvasElement>;
-  private context: CanvasRenderingContext2D | null = null;
-  private mouse : Mouse;
-  private canvasRect : DOMRect | null = null;
-  private linesList : Line[];
-  private archiveLinesList : Line[];
-  private pointsList : Point[];
-  private archivePointsList : Point[];
-  constructor() {
-    this.mouse = new Mouse();
-    this.linesList = [];
-    this.archiveLinesList = [];
-    this.pointsList = [];
-    this.archivePointsList = [];
-  }
-  ngAfterViewInit(): void {
-    this.context = this.canvas?.nativeElement.getContext('2d');
-    this.setCanvasSize();
-    this.canvasRect = this.canvas.nativeElement.getBoundingClientRect();
-    this.mouse.setCanvasRectFromDomRect(this.canvasRect);
-  }
+    @ViewChild('myCanvas', {static: true}) private canvas!: ElementRef<HTMLCanvasElement>;
+    private context: CanvasRenderingContext2D | null = null;
+    private mouse: Mouse;
+    private canvasRect: DOMRect | null = null;
+    private linesList: Line[];
+    private archiveLinesList: Line[];
+    private pointsList: Point[];
+    private archivePointsList: Point[];
 
-  onMouseDown(event: MouseEvent) : void {
-    this.mouse.mouseDown(event);
-    let point : Point = new Point(this.mouse.clickedCoordinates!.x - 2, this.mouse.clickedCoordinates!.y - 2);
-    this.drawPoint(point);
-    this.pointsList.push(point);
-  }
-
-  onMouseMove(event: MouseEvent) : void {
-    if(!this.mouse.isPressed)
-      return;
-    this.mouse.mouseMove(event);
-    if(this.mouse.notFirstMouseMoveEvent)
-      this.linesList.pop();
-    else
-      this.mouse.notFirstMouseMoveEvent = true;
-    this.linesList.push(new Line(this.mouse.clickedCoordinates!!, this.mouse.currentCoordinates!!));
-    this.drawAll();
-  }
-
-  onMouseUp(event: MouseEvent) : void {
-    this.mouse.mouseUp(event);
-    this.linesList.pop();
-    this.linesList.push(new Line(this.mouse.clickedCoordinates!!, this.mouse.currentCoordinates!!));
-    let point : Point = new Point(this.mouse.currentCoordinates!.x - 2, this.mouse.currentCoordinates!.y - 2);
-    this.drawPoint(point);
-    this.pointsList.push(point);
-  }
-
-  drawLine(line: Line) : void {
-    if (this.context) {
-      this.context.beginPath();
-      this.context.moveTo(line.firstPoint.x, line.firstPoint.y);
-      this.context.lineTo(line.secondPoint.x, line.secondPoint.y);
-      this.context.stroke();
-    } else {
-      console.error('Context is null.');
+    constructor() {
+        this.mouse = new Mouse();
+        this.linesList = [];
+        this.archiveLinesList = [];
+        this.pointsList = [];
+        this.archivePointsList = [];
     }
-  }
 
-  drawPoint(point: Point) : void {
-    if (this.context) {
-      this.context.fillRect(point.x, point.y,5,5);
-    } else {
-      console.error('Context is null.');
+    ngAfterViewInit(): void {
+        this.context = this.canvas?.nativeElement.getContext('2d');
+        this.setCanvasSize();
+        this.canvasRect = this.canvas.nativeElement.getBoundingClientRect();
+        this.mouse.setCanvasRectFromDomRect(this.canvasRect);
     }
-  }
 
-  drawAllLines() : void {
-    this.linesList.forEach((line : Line): void => {
-      this.drawLine(line);
-    });
-  }
-  drawAllPoints() : void {
-    this.pointsList.forEach((point : Point): void => {
-      this.drawPoint(point);
-    });
-  }
-  drawAll() : void {
-    this.clear();
-    this.drawAllLines();
-    this.drawAllPoints();
-  }
-
-  undo() : void {
-    if(!this.containsElements())
-      return;
-    let line : Line | undefined = this.linesList.pop();
-    if(line != undefined) {
-      this.archiveLinesList.push(line);
-      this.archivePointsList.push(this.pointsList.pop()!!);
+    onMouseDown(event: MouseEvent): void {
+        this.mouse.setCurrentCoordinatesFromEvent(event);
+        if (this.mouse.drawing)
+            this.addLine(new Line(this.mouse.clickedCoordinates!!, this.mouse.currentCoordinates!!))
+        this.mouse.mouseDown(event);
+        let point: Point = new Point(this.mouse.clickedCoordinates!.x - 2, this.mouse.clickedCoordinates!.y - 2);
+        this.drawPoint(point);
+        this.pointsList.push(point);
     }
-    this.archivePointsList.push(this.pointsList.pop()!!);
-    this.drawAll();
-  }
 
-  redo() : void {
-    if(!this.containsArchivedElements())
-      return;
-    let line : Line | undefined = this.archiveLinesList.pop();
-    if(line != undefined)
-    {
-      this.linesList.push(line);
-      this.pointsList.push(this.archivePointsList.pop()!!);
-
+    onMouseMove(event: MouseEvent): void {
+        if (!this.mouse.drawing)
+            return;
+        this.mouse.mouseMove(event);
+        if (this.mouse.notFirstMouseMoveEvent)
+            this.linesList.pop();
+        else
+            this.mouse.notFirstMouseMoveEvent = true;
+        this.linesList.push(new Line(this.mouse.clickedCoordinates!!, this.mouse.currentCoordinates!!));
+        this.drawAll();
     }
-    this.pointsList.push(this.archivePointsList.pop()!!);
-    this.drawAll();
-  }
-  containsArchivedElements() : boolean {
-    return this.archivePointsList.length > 0 || this.archiveLinesList.length > 0;
-  }
-  containsElements() : boolean {
-    return this.pointsList.length > 0 || this.linesList.length > 0;
-  }
 
-  clear() : void {
-    this.context!.clearRect(0,0,this.canvasRect!.width,this.canvasRect!.height);
-  }
-
-  setCanvasSize(): void {
-    if (this.context) {
-      // Set canvas dimensions to match window size
-      this.canvas.nativeElement.width = window.innerWidth;
-      this.canvas.nativeElement.height = window.innerHeight;
+    drawLine(line: Line): void {
+        if (this.context) {
+            this.context.beginPath();
+            this.context.moveTo(line.firstPoint.x, line.firstPoint.y);
+            this.context.lineTo(line.secondPoint.x, line.secondPoint.y);
+            this.context.stroke();
+        } else {
+            console.error('Context is null.');
+        }
     }
-  }
+
+    addLine(line: Line): void {
+        this.linesList.pop();
+        this.linesList.push(line);
+    }
+
+    drawPoint(point: Point): void {
+        if (this.context) {
+            this.context.fillRect(point.x, point.y, 5, 5);
+        } else {
+            console.error('Context is null.');
+        }
+    }
+
+    drawAllLines(): void {
+        this.linesList.forEach((line: Line): void => {
+            this.drawLine(line);
+        });
+    }
+
+    drawAllPoints(): void {
+        this.pointsList.forEach((point: Point): void => {
+            this.drawPoint(point);
+        });
+    }
+
+    drawAll(): void {
+        this.clear();
+        this.drawAllLines();
+        this.drawAllPoints();
+    }
+
+    undo(): void {
+        if (!this.containsElements())
+            return;
+        let line: Line | undefined = this.linesList.pop();
+        if (line != undefined) {
+            this.archiveLinesList.push(line);
+            this.archivePointsList.push(this.pointsList.pop()!!);
+        }
+        this.archivePointsList.push(this.pointsList.pop()!!);
+        this.drawAll();
+    }
+
+    redo(): void {
+        if (!this.containsArchivedElements())
+            return;
+        let line: Line | undefined = this.archiveLinesList.pop();
+        if (line != undefined) {
+            this.linesList.push(line);
+            this.pointsList.push(this.archivePointsList.pop()!!);
+
+        }
+        this.pointsList.push(this.archivePointsList.pop()!!);
+        this.drawAll();
+    }
+
+    containsArchivedElements(): boolean {
+        return this.archivePointsList.length > 0 || this.archiveLinesList.length > 0;
+    }
+
+    containsElements(): boolean {
+        return this.pointsList.length > 0 || this.linesList.length > 0;
+    }
+
+    clear(): void {
+        this.context!.clearRect(0, 0, this.canvasRect!.width, this.canvasRect!.height);
+    }
+
+    setCanvasSize(): void {
+        if (this.context) {
+            // Set canvas dimensions to match window size
+            this.canvas.nativeElement.width = window.innerWidth;
+            this.canvas.nativeElement.height = window.innerHeight;
+        }
+    }
+
+    @HostListener('document:keydown', ['$event'])
+    handleKeyDown(event: KeyboardEvent): void {
+        if (event.key === 'Escape') {
+            this.mouse.drawing = false;
+            if (this.mouse.moving) {
+                this.mouse.moving = false;
+                this.linesList.pop();
+            }
+            this.drawAll();
+        }
+    }
 }
--- src/app/models/mouse.ts
+++ src/app/models/mouse.ts
@@ -1,14 +1,16 @@
-import { Point } from "./point";
+import {Point} from "./point";
 
 export class Mouse {
-    private _isPressed: boolean;
+    private _drawing: boolean;
+    private _moving: boolean;
     private _notFirstMouseMoveEvent: boolean;
     private _clickedCoordinates: Point | null;
     private _currentCoordinates!: Point | null;
-    private _canvasRect : Point | null;
+    private _canvasRect: Point | null;
 
     constructor() {
-        this._isPressed = false;
+        this._drawing = false;
+        this._moving = false;
         this._notFirstMouseMoveEvent = false;
         this._clickedCoordinates = null;
         this._currentCoordinates = null;
@@ -16,14 +18,25 @@ export class Mouse {
     }
 
     // Getter for isPressed property
-    get isPressed(): boolean {
-        return this._isPressed;
+    get drawing(): boolean {
+        return this._drawing;
     }
 
     // Setter for isPressed property
-    set isPressed(value: boolean) {
-        this._isPressed = value;
+    set drawing(value: boolean) {
+        this._drawing = value;
     }
+
+    // Getter for isPressed property
+    get moving(): boolean {
+        return this._moving;
+    }
+
+    // Setter for isPressed property
+    set moving(value: boolean) {
+        this._moving = value;
+    }
+
     // Getter for firstMouseMoveEvent property
     get notFirstMouseMoveEvent(): boolean {
         return this._notFirstMouseMoveEvent;
@@ -45,7 +58,7 @@ export class Mouse {
     }
 
     // Getter for currentCoordinates property
-    get currentCoordinates(): Point | null{
+    get currentCoordinates(): Point | null {
         return this._currentCoordinates;
     }
 
@@ -54,8 +67,15 @@ export class Mouse {
         this._currentCoordinates = value;
     }
 
+    // Setter for currentCoordinates property
+    setCurrentCoordinatesFromEvent(event: MouseEvent): void {
+        this._currentCoordinates = new Point(
+            event.clientX - this._canvasRect!.x, event.clientY - this._canvasRect!.y
+        );
+    }
+
     // Getter for currentCoordinates property
-    get canvasRect(): Point | null{
+    get canvasRect(): Point | null {
         return this._canvasRect;
     }
 
@@ -69,25 +89,16 @@ export class Mouse {
         this._canvasRect = new Point(rect.left, rect.top);
     }
 
-
-
-    mouseDown = (event: MouseEvent) : void =>
-    {
-        this._isPressed = true;
+    mouseDown = (event: MouseEvent): void => {
+        this._drawing = true;
+        this._moving = false;
         this._clickedCoordinates = new Point(
             event.clientX - this._canvasRect!.x, event.clientY - this._canvasRect!.y
         );
-    }
-    mouseUp = (event: MouseEvent) : void =>
-    {
-        this._isPressed = false;
         this._notFirstMouseMoveEvent = false;
-        this._currentCoordinates = new Point(
-            event.clientX - this._canvasRect!.x, event.clientY - this._canvasRect!.y
-        );
     }
-    mouseMove = (event: MouseEvent) : void =>
-    {
+    mouseMove = (event: MouseEvent): void => {
+        this._moving = true;
         this._currentCoordinates = new Point(
             event.clientX - this._canvasRect!.x, event.clientY - this._canvasRect!.y
         );
