fccf9b55509d238f6a6f5aea3b0746046024939b
Rayan MARMAR <rayan.marmar@etu.unistra.fr>
2024-01-17 15:25:33 +0100

    :bug: fix grid import bug

--- src/app/components/app.component.ts
+++ src/app/components/app.component.ts
@@ -1,13 +1,12 @@
 import {Component} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {RouterOutlet} from '@angular/router';
-import {GridComponent} from "./grid/grid.component";
 import {MainBodyComponent} from "./main-body/main-body.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [CommonModule, RouterOutlet, GridComponent, MainBodyComponent],
+    imports: [CommonModule, RouterOutlet, MainBodyComponent],
     templateUrl: './app.component.html',
     styleUrl: './app.component.css'
 })
