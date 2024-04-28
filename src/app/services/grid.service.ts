4b08764df89af9603cd2910ad144243875592614
Rayan MARMAR <rayan.marmar@etu.unistra.fr>
2024-04-28 15:46:46 +0200

    :bug: fix snap grid bug

--- src/app/services/grid.service.ts
+++ src/app/services/grid.service.ts
@@ -27,10 +27,12 @@ export class GridService {
     calculateNearestGridIntersection(point: Point): Point {
         if (this.gridComponent != null) {
             const gridSize = this.gridComponent.squareSize * this.transformationService.scaleValue; // Adjust grid size based on zoom level
+            let xDelta = (this.transformationService.transformationMatrix[0][2]) % gridSize;
+            let yDelta = (this.transformationService.transformationMatrix[1][2]) % gridSize;
 
             // Calculate the nearest intersection coordinates
-            let x = Math.round(point.x / gridSize) * gridSize;
-            let y = Math.round(point.y / gridSize) * gridSize;
+            let x = Math.round(point.x / gridSize) * gridSize + xDelta;
+            let y = Math.round(point.y / gridSize) * gridSize + yDelta;
 
 
             return new Point(x, y);
