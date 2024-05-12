df6ecf0896edab81cda029378387adb72cee5ade
Rayan MARMAR <rayan.marmar@etu.unistra.fr>
2024-05-12 16:40:37 +0200

    :recycle: refactor wall object

--- src/app/drawables/wall.ts
+++ src/app/drawables/wall.ts
@@ -5,10 +5,6 @@ import {v4 as uuidv4} from 'uuid';
 
 export class Wall implements Drawable {
 
-    private _firstPoint: Point;
-    private _secondPoint: Point;
-    private _thirdPoint: Point;
-    private _fourthPoint: Point;
     private _firstLine: Line;
     private _secondLine: Line;
     private _thirdLine: Line;
@@ -18,6 +14,7 @@ export class Wall implements Drawable {
     private _xFactor: number;
     private _yFactor: number;
     private _uid: string;
+    private range: number = 10;
 
     constructor(
         firstPoint: Point,
@@ -26,66 +23,30 @@ export class Wall implements Drawable {
         reverseTransformationMatrix: number[][],
         uid: string | null
     ) {
+        // Get the original point coordinates in case of a certain transformation
         firstPoint = firstPoint.transform(reverseTransformationMatrix);
         secondPoint = secondPoint.transform(reverseTransformationMatrix);
+
+        // Initialize variables
         this._height = height;
         this._yFactor = (firstPoint.y - secondPoint.y) >= 0 ? -1 : 1;
         this._xFactor = (firstPoint.x - secondPoint.x) >= 0 ? -1 : 1;
+
+        // Calculate wall lines
         this._firstLine = new Line(firstPoint, secondPoint)
             .calculateParallelLine(this._height / 2, this._xFactor, this._yFactor, -1);
         this._thirdLine = this._firstLine.calculateParallelLine(this._height, this._xFactor, this._yFactor);
-        this._firstPoint = this._firstLine.firstPoint;
-        this._secondPoint = this._firstLine.secondPoint;
-        this._thirdPoint = this._thirdLine.secondPoint;
-        this._fourthPoint = this._thirdLine.firstPoint;
-        this._secondLine = new Line(this._secondPoint, this._thirdPoint);
-        this._fourthLine = new Line(this._fourthPoint, this._firstPoint);
+        this._secondLine = new Line(this._firstLine.secondPoint, this._thirdLine.secondPoint);
+        this._fourthLine = new Line(this._thirdLine.firstPoint, this._firstLine.firstPoint);
+
+        // Get wall width
         this._width = this._firstLine.calculateDistance();
+        // Initialize new UID for new walls
         this._uid = uid ?? uuidv4();
     }
 
     get uid(): string {
         return this._uid;
-    }
-
-    // Getter for firstPoint
-    get firstPoint(): Point {
-        return this._firstPoint;
-    }
-
-    // Setter for firstPoint
-    set firstPoint(value: Point) {
-        this._firstPoint = value;
-    }
-
-    // Getter for secondPoint
-    get secondPoint(): Point {
-        return this._secondPoint;
-    }
-
-    // Setter for secondPoint
-    set secondPoint(value: Point) {
-        this._secondPoint = value;
-    }
-
-    // Getter for thirdPoint
-    get thirdPoint(): Point {
-        return this._thirdPoint;
-    }
-
-    // Setter for thirdPoint
-    set thirdPoint(value: Point) {
-        this._thirdPoint = value;
-    }
-
-    // Getter for fourthPoint
-    get fourthPoint(): Point {
-        return this._fourthPoint;
-    }
-
-    // Setter for fourthPoint
-    set fourthPoint(value: Point) {
-        this._fourthPoint = value;
     }
 
     // Getter for firstLine
@@ -152,56 +113,13 @@ export class Wall implements Drawable {
         this._yFactor = value;
     }
 
-    isLineExtremity(point: Point): boolean {
-        return this._firstPoint.equals(point) || this._secondPoint.equals(point)
-            || this._thirdPoint.equals(point) || this._fourthPoint.equals(point);
-    }
-
     toString(): string {
-        return "{a = " + this._firstPoint.toString() + ", b = " + this._secondPoint.toString()
-            + ", c = " + this._thirdPoint.toString() + ", d = " + this._fourthPoint.toString() + "}";
+        return "{a = " + this._firstLine.firstPoint.toString() + ", b = " + this._firstLine.secondPoint.toString()
+            + ", c = " + this._thirdLine.secondPoint.toString() + ", d = " + this._thirdLine.firstPoint.toString() + "}";
     }
 
     containsPoint(point: Point): boolean {
-        const x = point.x;
-        const y = point.y;
-
-        // Check if the point is inside the polygon formed by the wall's vertices
-        // Using the winding number algorithm
-        const windingNumber = this.calculateWindingNumber(x, y);
-
-        // If the winding number is non-zero, the point is inside the polygon
-        return windingNumber !== 0;
-    }
-
-    private calculateWindingNumber(x: number, y: number): number {
-        // Calculate the winding number using the vertices of the wall
-        let windingNumber = 0;
-
-        windingNumber += this.calculateWindingNumberForEdge(this._firstPoint, this._secondPoint, x, y);
-        windingNumber += this.calculateWindingNumberForEdge(this._secondPoint, this._thirdPoint, x, y);
-        windingNumber += this.calculateWindingNumberForEdge(this._thirdPoint, this._fourthPoint, x, y);
-        windingNumber += this.calculateWindingNumberForEdge(this._fourthPoint, this._firstPoint, x, y);
-
-        return windingNumber;
-    }
-
-    private calculateWindingNumberForEdge(start: Point, end: Point, x: number, y: number): number {
-        // Calculate the winding number for a given edge and point
-        if (start.y <= y) {
-            if (end.y > y && this.isLeft(start, end, x, y) > 0) {
-                return 1;
-            }
-        } else if (end.y <= y && this.isLeft(start, end, x, y) < 0) {
-            return -1;
-        }
-
-        return 0;
-    }
-
-    private isLeft(start: Point, end: Point, x: number, y: number): number {
-        // Helper function to determine if a point is to the left of an edge
-        return ((end.x - start.x) * (y - start.y)) - ((x - start.x) * (end.y - start.y));
+        return this.calculateNearestPointDistance(point) < this.range;
     }
 
     calculateNearestPointDistance(point: Point): number {
@@ -222,10 +140,10 @@ export class Wall implements Drawable {
         let wall: Wall = this.transform(transformationMatrix);
         // Draw a filled rectangle with the correct coordinates
         context.beginPath();
-        context.moveTo(wall.firstPoint.x, wall.firstPoint.y);
-        context.lineTo(wall.secondPoint.x, wall.secondPoint.y);
-        context.lineTo(wall.thirdPoint.x, wall.thirdPoint.y);
-        context.lineTo(wall.fourthPoint.x, wall.fourthPoint.y);
+        context.moveTo(wall._firstLine.firstPoint.x, wall._firstLine.firstPoint.y);
+        context.lineTo(wall._firstLine.secondPoint.x, wall._firstLine.secondPoint.y);
+        context.lineTo(wall._thirdLine.secondPoint.x, wall._thirdLine.secondPoint.y);
+        context.lineTo(wall._thirdLine.firstPoint.x, wall._thirdLine.firstPoint.y);
         context.closePath();
         context.fillStyle = wallColor;
         context.strokeStyle = wallColor;
